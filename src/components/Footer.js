2325294cc4d0ef76da5eefb2cf53192ea08318d6
AR123456 <coloradoz99@gmail.com>
2022-01-25 07:03:53 -0700

    change over to reactstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { Container, Col, Row, Card, Navbar } from "react-bootstrap";
+import { Container, Col, Row, Card, Navbar } from "reactstrap";
 
 const Footer = () => {
   return (
     <>
-      <Card body>
-        <Navbar>
-          <Container>
-            <Navbar.Brand href="#home">Copyright 2020</Navbar.Brand>
-            <Navbar.Brand href="#privacy">Privacy Policy</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                Social Icon links <a href="#twitter">Twitter </a>
-              </Navbar.Text>
-              <Navbar.Text>
-                Social Icon links <a href="#Facebook">Facebook </a>
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </Card>
+      <Container>
+        <Row>
+          <Col className="bg-light border">Copyright 2022</Col>
+        </Row>
+        <Row>
+          {/* <Col className="bg-light border">.col</Col>
+          <Col className="bg-light border">.col</Col>
+          <Col className="bg-light border">.col</Col>
+          <Col className="bg-light border">.col</Col> */}
+        </Row>
+
+        <Row>
+          <Col className="bg-light border" xs="6">
+            Privacy Policy
+          </Col>
+          <Col className="bg-light border" xs="6">
+            Social
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,84 +2,50 @@ import React from "react";
 import {
   Navbar,
   Nav,
-  Container,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-  Row,
-  Col,
-} from "react-bootstrap";
+  NavbarBrand,
+  NavbarToggler,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  NavbarText,
+  Collapse,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">CEU's For You Inc </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            <Navbar.Text>
-              <Nav className="me-auto">
-                <Form className="d-flex">
-                  <Row className="align-items-center">
-                    <Col sm={3} className="my-1">
-                      <FormControl
-                        type="email"
-                        placeholder="Enter email"
-                        className="me-2"
-                        aria-label="Search"
-                      />
-                    </Col>
-                    <Col sm={3} className="my-1">
-                      <Form.Control type="password" placeholder="Password" />
-                    </Col>
-                    <Col xs="auto" className="my-1">
-                      <Form.Check
-                        type="checkbox"
-                        id="autoSizingCheck2"
-                        label="Remember me"
-                      />
-                    </Col>
-                    <Col xs="auto" className="my-1">
-                      <Button type="submit">Submit</Button>
-                    </Col>
-                    <Col xs="auto" className="my-1">
-                      <Nav.Link href="#home">Forgot Password</Nav.Link>
-                    </Col>
-                  </Row>
-                </Form>
-
-                <NavDropdown title="More" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#catalog">Catalog</NavDropdown.Item>
-                  <NavDropdown.Item href="#inperson">
-                    In Person Learning
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#web-based">
-                    Live Web based
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#web-based">
-                    Self Serve On Demand
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#mystuf">My Courses</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#contact">
-                    Contact Us
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Log In</NavDropdown.Item>
-                </NavDropdown>
-
-                <Nav.Link href="#home">Home</Nav.Link>
-              </Nav>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div>
+        <Navbar color="light" expand="md" light>
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() {}} />
+          <Collapse navbar>
+            <Nav className="me-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown inNavbar nav>
+                <DropdownToggle caret nav>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
     </>
   );
 };
