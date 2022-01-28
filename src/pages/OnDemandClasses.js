addce23a71e51efafc1258eabaefce46c56d5d27
AR123456 <coloradoz99@gmail.com>
2022-01-28 06:46:42 -0700

    implementing jumbtron on pages

--- src/pages/OnDemandClasses.js
+++ src/pages/OnDemandClasses.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 const OnDemandClasses = () => {
   return (
-    <div>
-      <h1>Look here for articles to read and test your knowlagde for CEUs </h1>
-    </div>
+    <>
+      <Container>
+        <Navigation></Navigation>
+        <div className="h-100 p-5 bg-light border rounded-3">
+          <div className="container-fluid bg-light text-dark p-5">
+            <div className="container bg-light p-5">
+              <h1 className="display-4 fw-bold">List of on demand articles </h1>
+            </div>
+          </div>
+        </div>
+        <Footer></Footer>
+      </Container>
+    </>
   );
 };
 
--- src/pages/WebinarClasses.js
+++ src/pages/WebinarClasses.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 const WebinarClasses = () => {
   return (
-    <div>
-      <h1>Look at these opportunitys to Zoom into our lectures </h1>
-    </div>
+    <>
+      <Container>
+        <Navigation></Navigation>
+        <div className="h-100 p-5 bg-light border rounded-3">
+          <div className="container-fluid bg-light text-dark p-5">
+            <div className="container bg-light p-5">
+              <h1 className="display-4 fw-bold">List of webinars </h1>
+            </div>
+          </div>
+        </div>
+        <Footer></Footer>
+      </Container>
+    </>
   );
 };
 
