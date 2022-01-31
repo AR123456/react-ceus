275ee63c9d95a7ece2da466e42afb628d8a89234
AR123456 <coloradoz99@gmail.com>
2022-01-31 06:07:22 -0700

    on demand page

--- src/pages/WebinarClasses.js
+++ src/pages/WebinarClasses.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container, ListGroup, ListGroupItem } from "reactstrap";
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Col,
+  Table,
+} from "reactstrap";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 
@@ -12,13 +19,53 @@ const WebinarClasses = () => {
           <div className="container-fluid bg-light text-dark p-5">
             <div className="container bg-light p-5">
               <h1 className="display-4 fw-bold">List of webinars </h1>
-              <ListGroup>
-                <ListGroupItem>Cras justo odio</ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Morbi leo risus</ListGroupItem>
-                <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                <ListGroupItem>Vestibulum at eros</ListGroupItem>
-              </ListGroup>
+              <Table responsive hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>CEUs</th>
+                    <th>Type</th>
+                    <th>Cost</th>
+                    <th>Description</th>
+                    <th>More Info</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Cras justo odio</td>
+                    <td>Dapibus ac facilisis in</td>
+                    <td>Morbi leo risus</td>
+                    <td>Porta ac consectetur a</td>
+                    <td>Vestibulum at eros</td>
+                    <td>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quod, dolorem consequuntur? Asperiores voluptate eligendi
+                      officia ipsum corrupti, aut sapiente natus nemo. Ipsam
+                      doloremque eum id, quam enim porro voluptates asperiores.
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+                </tbody>
+              </Table>
             </div>
           </div>
         </div>
