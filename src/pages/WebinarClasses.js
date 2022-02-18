b8b5ac8597df67e932a613bc5be473a501c128a3
AR123456 <coloradoz99@gmail.com>
2022-02-18 06:32:22 -0700

    implementing singleclass component

--- src/pages/WebinarClasses.js
+++ src/pages/WebinarClasses.js
@@ -1,157 +1,17 @@
 import React from "react";
-import {
-  Container,
-  Table,
-  UncontrolledPopover,
-  PopoverBody,
-  Button,
-  Input,
-  InputGroup,
-  ListGroup,
-  ListGroupItem,
-  PopoverHeader,
-} from "reactstrap";
+
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import SingleClass from "../components/SingleClass";
 
 const WebinarClasses = () => {
   return (
     <>
-      <Container>
-        <Navigation></Navigation>
-        <div className="h-100 p-5 bg-light border rounded-3">
-          {/* <div className="container-fluid bg-light text-dark p-5"> */}
-          {/* <div className="container bg-light p-5"> */}
-          <h1 className="display-4 fw-bold">List of webinars </h1>
-          {/* implement filtering based on description search  */}
-          <Input
-            id="exampleSearch"
-            name="search"
-            placeholder="Search all webinars"
-            type="search"
-          />
-          <Table responsive hover>
-            <thead>
-              <tr>
-                <th>Select</th>
-                <th>Title</th>
-                <th>CEUs</th>
-                <th>Type</th>
-                <th>Cost</th>
-                <th>Description</th>
-                <th>More Info</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">
-                  {" "}
-                  <Input
-                    addon
-                    aria-label="Checkbox for following text input"
-                    type="checkbox"
-                  />
-                </th>
-                <td>PPE Basics</td>
-                <td>1.2</td>
-                <td>Webinar</td>
-                <td>$65.00</td>
-                <td>Donning and Doffing PPE safely </td>
-                <td>
-                  <Button id="PopoverFocus" type="button">
-                    Click for more info
-                  </Button>{" "}
-                  <UncontrolledPopover
-                    placement="bottom"
-                    target="PopoverFocus"
-                    // trigger="focus"
-                    trigger="click"
-                  >
-                    <PopoverBody>
-                      <PopoverHeader>
-                        <h4>Description</h4>
-                        Sequence for donning and doffing personal protective
-                        equipment (PPE) Donning and doffing is the practice of
-                        employees putting on and removing work-related
-                        protective gear, clothing, and uniforms. Donning refers
-                        to putting on work clothes, gear, and equipment, while
-                        doffing means removing them.
-                      </PopoverHeader>
+      <Navigation></Navigation>
+      {/* loop class data show each class that is a webinar , do same for other pages but just if on demand or in person */}
+      <SingleClass></SingleClass>
 
-                      <ListGroup numbered>
-                        <h4>Includes</h4>
-                        <ListGroupItem>Cras justo odio</ListGroupItem>
-                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                        <ListGroupItem>Morbi leo risus</ListGroupItem>
-                        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                      </ListGroup>
-                    </PopoverBody>
-                  </UncontrolledPopover>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">
-                  {" "}
-                  <Input
-                    addon
-                    aria-label="Checkbox for following text input"
-                    type="checkbox"
-                  />
-                </th>
-                <td>PPE Basics</td>
-                <td>1.2</td>
-                <td>Webinar</td>
-                <td>$65.00</td>
-                <td>Donning and Doffing PPE safely </td>
-                <td>
-                  <Button id="PopoverFocus" type="button">
-                    Click for more info
-                  </Button>{" "}
-                  <UncontrolledPopover
-                    placement="bottom"
-                    target="PopoverFocus"
-                    // trigger="focus"
-                    trigger="click"
-                  ></UncontrolledPopover>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">
-                  {" "}
-                  <Input
-                    addon
-                    aria-label="Checkbox for following text input"
-                    type="checkbox"
-                  />
-                </th>
-                <td>PPE Basics</td>
-                <td>1.2</td>
-                <td>Webinar</td>
-                <td>$65.00</td>
-                <td>Donning and Doffing PPE safely </td>
-                <td>
-                  <Button id="PopoverFocus" type="button">
-                    Click for more info
-                  </Button>{" "}
-                  <UncontrolledPopover
-                    placement="bottom"
-                    target="PopoverFocus"
-                    // trigger="focus"
-                    trigger="click"
-                  ></UncontrolledPopover>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-          <InputGroup>
-            <Button>Add selected to cart</Button>
-          </InputGroup>
-        </div>
-        <br />
-        <div></div>
-        <Footer></Footer>
-      </Container>
+      <Footer></Footer>
     </>
   );
 };
