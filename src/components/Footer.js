ee57cf6d093b074ce7e62c322c7842e0449e1c60
AR123456 <coloradoz99@gmail.com>
2022-02-18 06:34:19 -0700

    react-bootstrap-cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { Col, Row, Card, Navbar } from "reactstrap";
+import { Container, Card } from "react-bootstrap";
+import { Col, Row, Navbar } from "reactstrap";
 import { FaFacebookF } from "react-icons/fa";
 import { CgTwitter } from "react-icons/cg";
 import { AiFillCopyrightCircle } from "react-icons/ai";
