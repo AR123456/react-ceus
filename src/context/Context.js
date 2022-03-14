b3c61a3f58151e2d946be68f87febb02e9c72d5d
AR123456 <coloradoz99@gmail.com>
2022-03-14 04:33:36 -0600

    reducer and context set up

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,31 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import ceuOfferings from "./CEUdata.js";
-console.log(ceuOfferings);
+import { cartReducer } from "./Reducers.js";
+
+// console.log(ceuOfferings);
 // Cart is the name of the context or state
 const Cart = createContext();
 // pass in the children coming from index.js, which is the point where the app starts - gooto index.js  and wrap it in context
 // are children aka props ?
 const Context = ({ children }) => {
-  // in here get the data - for now just from data.js
-  // const ceus = [...Array(3)].map(() => ({
-  //   ////
-  //   id: ceuOfferings.id,
-  //   title: ceuOfferings.title,
-  //   CEU: ceuOfferings.CEU,
-  //   type: ceuOfferings.type,
-  //   cost: ceuOfferings.cost,
-  //   shortDescription: ceuOfferings.shortDescription,
-  //   longDescription: ceuOfferings.longDescription,
-  //   featured: ceuOfferings.featured,
-  // }));
-
-  // why is this undefined ????
-  // console.log(ceus);
-
   //  here just passing in the data from ceuOfferings.js
   const ceus = ceuOfferings;
+  // setting up state for all the ceus and the ceus in cart
+  // cartReducer is comming from Reducers.js
+  const [state, dispatch] = useReducer(cartReducer, {
+    ceus: ceus,
+    cart: [],
+  });
 
-  return <Cart.Provider value={{ ceus }}>{children}</Cart.Provider>;
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 };
 
 export default Context;
+export const CartState = () => {
+  return useContext(Cart);
+};
